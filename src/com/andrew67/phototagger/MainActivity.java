f7f6706c243201a6d3ab5d0c873d8c472f33bdb4
Andrew67 <andres@andrew67.com>
2013-04-23 16:19:03 -0400

    Add menu option that allows selecting an image from a Gallery application instead of the grid provided by the application.

--- src/com/andrew67/phototagger/MainActivity.java
+++ src/com/andrew67/phototagger/MainActivity.java
@@ -1,10 +1,14 @@
 package com.andrew67.phototagger;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -12,6 +16,8 @@ import android.widget.GridView;
 
 public class MainActivity extends Activity {
 	
+	private static final int ACTIVITY_SELECT_IMAGE = 0;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,5 +46,46 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("MainActivity", "menu: " + item.getItemId());
+		switch (item.getItemId()) {
+		// Menu item for selecting an image using a gallery application
+		case R.id.action_gallery:
+			final Intent i = new Intent(Intent.ACTION_PICK,
+		               android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, 
+		       Intent returnedIntent) {
+		    super.onActivityResult(requestCode, resultCode, returnedIntent); 
+
+		    switch(requestCode) { 
+		    // Handle image selected from gallery application instead of our grid
+		    case ACTIVITY_SELECT_IMAGE:
+		        if(resultCode == RESULT_OK){  
+		            final Uri selectedImage = returnedIntent.getData();
+		            final String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+		            final Cursor cursor = getContentResolver().query(
+		                               selectedImage, filePathColumn, null, null, null);
+		            cursor.moveToFirst();
+
+		            final int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+		            final String imgPath = cursor.getString(columnIndex);
+		            cursor.close();
+
+					final Intent i = new Intent(MainActivity.this, TagActivity.class);
+					i.putExtra("imgPath", imgPath);
+					Log.d("MainActivity", "path: " + imgPath);
+					startActivity(i);
+		        }
+		    }
+		}
 
 }
