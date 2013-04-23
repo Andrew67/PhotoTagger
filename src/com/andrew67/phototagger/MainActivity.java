72d29852b72b0dbd90d983c26043440531996675
Andrew67 <andres@andrew67.com>
2013-04-23 03:08:02 -0400

    Set up GridView on MainActivity Set theme to Holo Dark Load grid with thumbnails of images (is slow when generating missing thumbnails while scrolling)

--- src/com/andrew67/phototagger/MainActivity.java
+++ src/com/andrew67/phototagger/MainActivity.java
@@ -2,14 +2,36 @@ package com.andrew67.phototagger;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final GridView grid = (GridView) findViewById(R.id.gridview);
+		final ThumbnailAdapter thumbnails = new ThumbnailAdapter(this);
+		grid.setAdapter(thumbnails);
+		grid.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				final String imagePath = thumbnails.getImagePath(position);
+				//final Intent i = new Intent(MainActivity.this, TagActivity.class);
+				//i.putExtra("imagePath", imagePath);
+				Log.d("MainActivity", "path: " + imagePath);
+				//startActivity(i);
+			}
+		});
 	}
 
 	@Override
